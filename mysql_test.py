badad6c5d750315c4e321c72a20979b4b24e7671
shitou <shitou@bolo.em>
2020-09-17 18:07:19 +0800

    to mysql

--- mysql_test.py
+++ mysql_test.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import os
+import time
 import tushare as ts
 from sqlalchemy import create_engine
+from concurrent.futures import ThreadPoolExecutor
 
 engine_ts = create_engine('mysql://root:123456@localhost:3306/stock_data?charset=utf8&use_unicode=1')
 
@@ -21,10 +23,30 @@ def get_data():
                                        'list_status,list_date,list_date,is_hs')
     return df
 
+token = os.environ.get('tushare_token')
+pro = ts.pro_api(token)  # 初始化pro接口
+
+table_name = 'stock_day_data'
+
+start = '20200902'
+end = '20200916'
+def to_sql(ts_code):
+    print(ts_code)
+    data = pro.daily(ts_code=ts_code, start_date=start, end_date=end)
+    time.sleep(0.2)
+    # table_name = df.iloc[1, df.columns.get_loc('symbol')]
+    data.to_sql(table_name, engine_ts, index=False, if_exists='append')
+
+def mult_thread(df, count=8):
+    with ThreadPoolExecutor(max_workers=count) as executor:
+        executor.map(to_sql, df['ts_code'].values)
 
 if __name__ == '__main__':
     df = read_data()
     #df = get_data()
     #write_data(df, name='stock_basic')
-    for ts_code in df['ts_code'].values:
-        print(ts_code)
+    #print(df.iloc[1, df.columns.get_loc('symbol')])
+    mult_thread(df)
+
+    # for idx, ts_code in enumerate(df['ts_code'].values):
+    #     print(idx)
