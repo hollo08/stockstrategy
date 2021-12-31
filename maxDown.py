ac2aefa38c21ecf0a1fe78fd4687d7c6a6ebe866
shitou <shitou@bolo.em>
2021-12-31 18:27:11 +0800

    test

--- maxDown.py
+++ maxDown.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plot
 
-x = [32,35,66,88,32,34,23]
+x = [32,35,66,188,32,34,23,201,19]
 j = np.argmax(np.maximum.accumulate(x) - x)
-print('j is {j}'.format(j=j))
+print('j is {0}'.format(j))
 print(np.maximum.accumulate(x))
+print(np.maximum.accumulate(x)-x)
